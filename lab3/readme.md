9048e699647e747ed2826720a7ee42331b335ceb
yifanlu0227 <458887872@qq.com>
2021-04-10 09:01:04 -0700

    Finish TCP chat room

--- lab3/readme.md
+++ lab3/readme.md
@@ -1,7 +1,7 @@
 client.cpp 和 server.cpp 是TCP连接的初始版本，每次连接都要新的sock，将要写新的版本会保存新加入进来的客户端的sockfd
 
 ## Usage
-g++ server.cpp -o server
+g++ server.cpp -o server -lpthread
 g++ client.cpp -o client
 
 sudo mn --custom lab3-topo.py --topo lab3topo
--- lab3/server.cpp
+++ lab3/server.cpp
@@ -10,6 +10,7 @@
 #include<sys/wait.h>
 #include<arpa/inet.h>
 #include<netdb.h>
+#include<pthread.h>
 #include<map>
 
 #define PORT "3490"
@@ -19,11 +20,53 @@
 #define MAX_CLIENT 20
 using namespace std;
 
+map<string,int> recvfd_table;
+map<string,int> sendfd_table;
+
 void *get_in_addr(struct sockaddr *sa){
     if(sa->sa_family == AF_INET){
         return &(((struct sockaddr_in*)sa)->sin_addr);
     }
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+static void * pthread(void *arg){
+    char *ip_addr = (char *)arg;
+    char buf[LEN];
+    string client_ip = ip_addr;
+    int new_fd = recvfd_table[client_ip];
+    while(1){
+        memset(buf,'\0',LEN);
+        int recv_len = recv(new_fd,buf,LEN,0);
+
+        // indicating server have received the message
+        if(recv_len==-1){cout<<"receive failed."<<endl; break;}
+        else if(recv_len==0){cout<<"Closed by remote side."<<endl; 
+            close(sendfd_table[client_ip]);
+            close(recvfd_table[client_ip]);
+            sendfd_table.erase(client_ip);
+            recvfd_table.erase(client_ip);
+            break;
+        }
+        else {cout<<"Received:"<<buf<<endl;}
+
+        string text = buf;
+        string prefix = client_ip;
+        string sent_message = prefix + " says: "+ text;
+
+        
+        int sendfd;
+        // broadcast to other clients
+        for(map<string,int>::iterator iter=sendfd_table.begin();iter!=sendfd_table.end();iter++){
+            if(client_ip == iter->first)
+                continue;
+            sendfd = iter->second;
+            cout<<sent_message<<endl;
+            int bytes_sent = send(sendfd,sent_message.c_str(),sent_message.length(),0);
+            cout<<"sent byte "<<bytes_sent<<" to "<<iter->first<<" with sockfd "<<iter->second<<endl;
+        }
+    }
+    return NULL;
 }
 
 
@@ -32,8 +75,7 @@ int main(int argc,char *argv[]){
     // this is the old way
     struct addrinfo hints,*res;
     int sockfd,client_num=0;
-    map<string,int> recvfd_table;
-    map<string,int> sendfd_table;
+
 
 
     memset(&hints,0,sizeof hints);
@@ -84,46 +126,18 @@ int main(int argc,char *argv[]){
             char text[] = "Acknowledge from server. \0";
             int byte_send = send(sendfd,text,strlen(text),0);
             // cout<<"byte send in ack:"<<byte_send<<endl;
-        }
 
-        pid_t pid;
-        pid = fork();
 
-        if(pid==0){ // pid=0, child process, keep connection with new_fd above. Once received message, handout to other client
-            char buf[LEN];
-            string client_ip = ip_addr;
-            while(1){
-                memset(buf,'\0',LEN);
-                int recv_len = recv(new_fd,buf,LEN,0);
-
-                // indicating server have received the message
-                if(recv_len==-1){cout<<"receive failed."<<endl; break;}
-                else if(recv_len==0){cout<<"Closed by remote side."<<endl; 
-                    close(sendfd_table[client_ip]);
-                    close(recvfd_table[client_ip]);
-                    sendfd_table.erase(client_ip);
-                    recvfd_table.erase(client_ip);
-                    break;
-                }
-                else {cout<<"Received:"<<buf<<endl;}
-
-                string text = buf;
-                string prefix = client_ip;
-                string sent_message = prefix + " says: "+ text;
-
-                
-                int sendfd;
-                // broadcast to other clients
-                for(map<string,int>::iterator iter=sendfd_table.begin();iter!=sendfd_table.end();iter++){
-                    if(client_ip == iter->first)
-                        continue;
-                    sendfd = iter->second;
-                    cout<<sent_message<<endl;
-                    int bytes_sent = send(sendfd,sent_message.c_str(),sent_message.length(),0);
-                    cout<<"sent byte "<<bytes_sent<<" to "<<iter->first<<" with sockfd "<<iter->second<<endl;
-                }
+            pthread_t tid;
+            if ((pthread_create(&tid, NULL, pthread, s)) == -1){
+                cout<<("create error!\n");
+                return -1;
             }
+            cout<<"Thread created."<<endl;
         }
+
+
+        
 
     }
 
